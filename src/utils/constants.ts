0a6884b2d2ec59407d484dd7911646ae450a5889
Stenio Wagner <stenio.wagner1@gmail.com>
2024-08-03 15:56:44 -0300

    feat: creating the constants utility

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const messages = {
+  GET_TIMER: "GET_TIMER",
+  SET_STORAGE_ITEM: "SET_STORAGE_ITEM",
+  GET_STORAGE_ITEM: "GET_STORAGE_ITEM",
+};
+
+export const values = {
+  storage: {
+    TIMER_START: "TIMER_START",
+    TIMER_MINUTES: "TIMER_MINUTES",
+  },
+  timer: {
+    defaultTimer: 30, // 60 * 25,
+  },
+};
